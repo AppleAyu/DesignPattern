cfcc25966e591f5e089c7bb7be99c258dd7abf73
阿雨 <897682477@qq.com>
2019-12-29 07:40:20 +0800

    设计模式

---
+++ Iterator/src/tesst.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class tesst {
+    public static void main(String[] args) {
+        int arr[]=new int[20];
+
+    }
+}
+class Book{
+    String name;
+    public Book(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
